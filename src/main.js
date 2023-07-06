704c7e4d0cfa152817fd9b6628e6a1359e7ec1e7
Redhair Hambagu <redhair_hambagu@hotmail.com>
2023-07-06 16:37:41 +0000

    Updates

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
-import App from './App.vue'
-
-import './assets/main.css'
-
-createApp(App).use(ElementPlus).mount('#app')
